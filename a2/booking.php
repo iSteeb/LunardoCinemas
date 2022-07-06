bd831e5ff2d7143aa40922c7ac3b32983349a425
Steven Duzevich <u5824521@anu.edu.au>
2022-07-07 00:23:27 +1000

    a lot happened here, oops

--- a2/booking.php
+++ a2/booking.php
@@ -13,27 +13,48 @@
 
   <body>
 
-    <header>
-      <div>Put company logo and name here</div>
+    <header id="top">
+      <img id="logo" src="../../media/logo.png" alt="Lunardo logo" />
+      <h1 id="title">Lunardo</h1>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <a href="index.php">Home</a>
+      <a href="#top">Go to Top</a>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+      <section id='info'>
+        <h2>Info</h2>
+        <p>This page must have a consistent structure and style of the index page and show all of the details regarding the movie selected mentioned in the index page, along with a full synopsis of the movie, for one of the specified movies (your choice but the following details assumes the "ACT" Action film has been selected). 2 - 3 lead actors and director information should also be included here.<br>
+Tip: In the next assignment, the content will adapt based on the movie code supplied in the GET header from the index page.<br>
+A video trailer of the movie should be displayed which can either be an embedded video (eg in an iframe from a website like Youtube) or in a video element hosted from Coreteaching in your media folder. Make sure that this is not too big or too small for the type of screen that the user has.<br>
+Tip: Do not submit movie trailers or any media to Canvas as part of your assignment submission, all media must be present in your media folder, ie outside of your wp folder.</p>
+      </section>
+      <section id='book'>
+        <h2>Booking Form</h2>
+        <p>A usable and accessible form with clickable labels where appropriate, good layout and fieldset groups. It should contain and transmit:<br>
+* A hidden field to transmit the movie code, eg name="movie" and value="ACT" if the booking is for the action movie. An honest user should not be able to see or modify this field.<br>
+* Six drop down boxes for each of the seats group into an array, eg with name="seats[STA]" if this field is for the number of standard adult seats. The first option should be please select and have a blank value, other options should show integers between 1 and 10 and have a matching value. These fields should contain two data attributes that contain the full and discount price for each seat, eg data-fullprice="20.5" and data-discprice="15" for the standard adult seats. UPDATE: Previously a '-sta' seat-type substring was placed here, this was a mistake, you will need to remove it for assignment 3. <br>
+* Radio buttons and associated labels for each of the sessions that the movie is playing should be shown, but these should be styled so that the actual radio field is not shown and their associated labels look like 3D buttons in a raised state when not selected and a depressed state when selected. The radio buttons should all have the same name, ie name="day" and different values eg value="WED" if the movie is playing on a Wednesday. All buttons should have a data attribute that represents a full or discount session for the time it is playing, eg for movies playing on a Wednesday data-pricing="discprice" if playing at 12pm, data-pricing="fullprice" for all other times. <br>
+* Text-based fields of an appropriate type should collect the following user details and form submission should be blocked ONLY if these are left blank:<br>
+    * the full name of the customer, name="user[name]"<br>
+    * an email address, name="user[email]"<br>
+    * an Australian mobile number, name="user[mobile]"<br>
+The form should submit to itself (ie the booking page) using the post method.<br>
+Tip: The booking page already has an inbuilt debug area, it shows what has been submitted via GET and POST methods. Make sure that all fields arrive via the correct method and that no illegal values can be submitted by honest users.</p>
+      </section>
     </main>
+
     <footer>
+      <p>A footer element that contains the client's contact email, phone and address details (these can be dummy data). Please put your student name and student id in the footer with a link to your GitHub / BitBucket repository.</p>
       <div>&copy;<script>
         document.write(new Date().getFullYear());
       </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
+
     <aside id="debug">
       <hr>
       <h3>Debug Area</h3>
--- a2/index.php
+++ a2/index.php
@@ -1,39 +1,94 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Assignment 2</title>
     
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
-    <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
+    <link id="wireframecss" type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="style.css?t=<?= filemtime("style.css"); ?>">
-    <script src='../wireframe.js'></script>
+    <script src="../wireframe.js"></script>
+    <script>
+      document.addEventListener('scroll', function(e){
+        if (window.scrollY > document.getElementById("about").offsetTop) {
+          document.getElementById("go-to-top-button").style.display = "block";
+        } else {
+          document.getElementById("go-to-top-button").style.display = "none";
+        }
+
+      }, true);
+    </script>
   </head>
 
   <body>
-
-    <header>
-      <div>Put company logo and name here</div>
+    <header id="top">
+      <img id="logo" src="../../media/logo.png" alt="Lunardo logo" />
+      <h1 id="title">Lunardo</h1>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <a href="#about">about us</a>
+      <a href="#seats">our seats</a>
+      <a href="#showing">our movies</a>
+      <a href="#top" id="go-to-top-button">&uarr;</a>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
-      </article>
+      <section id="about">
+        <h2>about</h2>
+        <p>The client wants to target not only their loyal customer from before, but also local cinema goers who would normally drive to modern cinemas in nearby larger towns.
+This area should tell customers these things:<br>
+* The cinema has reopened after extensive improvements and renovations.<br>
+* There are new seats: standard seats and reclinable first class seats<br>
+* The projection and sound systems are upgraded with 3D Dolby Vision projection and Dolby Atmos sound.<br>
+* See https://professional.<wbr>dolby.com/cinema/ (Links to an external site.) and https://professional.<wbr>dolby.com/cinema/dolby-atmos (Links to an external site.) and http://www.dolby.com/us<wbr>/en/platforms/dolby-cinema.html for more information. (Links to an external site.)</p>
+      </section>
+      <section id="seats">
+        <h2>seats</h2>
+        <p>The client wants to show pictures of their new seats: standard seating and first class seating. Images for the seats are provided in the original repository.<br>
+The Cinema offers discounted pricing weekday afternoons (ie 12pm weekday matinée sessions) and all day on Mondays.<br>
+Seat Prices<br>
+Seat Type	Seat Code	Discounted Prices	Normal Prices<br>
+Standard Adult	STA	15.00	20.50<br>
+Standard Concession	STP	13.50	18.00<br>
+Standard Child	STC	12.00	16.50<br>
+First Class Adult	FCA	24.00	30.00<br>
+First Class Concession	FCP	22.50	27.00<br>
+First Class Child	FCC	21.00	24.00<br>
+The content above should be integrated and adapt nicely in all screen sizes but no explicit instructions have been provided. This is an analysis and implementation task for you to think about and you may discuss ideas with other students.</p>
+      </section>
+      <section id="showing">
+        <h2>showing</h2>
+        <p>This area should have 4 panels that shows details for each movie. These panels will have a "front" and a "back" side which will show different information (details below) and a "flip" effect should be triggered with a :focus pseudo state (and an optional :hover state if you wish).<br>
+* On the front: a movie poster, along with the name of the movie and the rating (eg PG, MA, R etc.). Tip: It will help if the poster images are all the same height and width as each other.<br>
+* On the back: a short synopsis of the movie plot should be show along with a list of day / times that the movie is playing, and a "book now" hyperlink which should be styled to look like a button. This link should take the user to a booking.php page along with a GET header containing the movie id, eg the action movie link should direct the user to "booking.php?movie=ACT". Tip: This url can be hard coded, but in the next assignment you are expected to create module code to generate these panels.<br>
+The content in the movie panels should adapt to large, medium and small screens, the layout for the front of each panel is described below:<br>
+* Small / Mobile View: The panels should be displayed "1 up", ie in a single column, internal contents should also be displayed in a single column with the movie poster below the title and rating.<br>
+* Medium / Tablet View: The panels should be displayed "1 up" as per the Small / Mobile View but the internal contents should be displayed in two columns, the poster on the left and the title and rating on the right.<br>
+* Large / Desktop View: The panels should be displayed "2 up", ie two on one row, with the internal contents displayed in two columns as per the Medium / Tablet View.<br>
+The content in the back of each panel should adapt nicely in all screen sizes but no explicit instructions have been provided. This is an analysis and implementation task for you to think about and you may discuss ideas with other students.<br>
+The table below shows which movies have been selected by the client and when they are playing:<br>
+Movies and Weekly Session Times<br>
+Movie Title	CODE	Mon - Tue	Wed - Fri	Sat - Sun	IMDB Link<br>
+Top Gun: Maverick	ACT	9pm	9pm	6pm	https://www.imdb.com/title/tt1745960 (Links to an external site.)<br>
+Mrs Harris goes to Paris	RMC	-	12pm	3pm	https://www.imdb.com/title/tt5151570 (Links to an external site.)<br>
+Lightyear	FAM	12pm	6pm	12pm	https://www.imdb.com/title/tt10298810 (Links to an external site.)<br>
+Prithviraj	AHF	6pm	-	9pm	https://www.imdb.com/title/tt9637132 (Links to an external site.)</p>
+      </section>
     </main>
 
     <footer>
-      <div>&copy;<script>
-        document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
-      <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
-      <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+      <div id="footer-cards">
+        <div class="footer-contact-card">Email:<br>enquiries@lunardo.com.au</div>
+        <div class="footer-contact-card">Phone:<br>(02) 6174 4290</div>
+        <div class="footer-contact-card">Address:<br>30/33 Hibberson St, Gungahlin</div>
+      </div>
+      <div id="footer-copyright">
+        <p>&copy; <script>document.write(new Date().getFullYear());</script> Steven Duzevich (s3963525) | Last modified <?= date ("Y F d  H:i", filemtime($_SERVER["SCRIPT_FILENAME"])); ?> | <a href="//github.com/isteeb/wp" target="_blank">GitHub Repo</a></p>
+        <p>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</p>
+        <p><a href="booking.php">Debugging Booking</a></p>
+      </div>
     </footer>
 
   </body>
