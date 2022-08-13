323fb0326d09a8ca4e7703d9734227ce3672121d
Steven Duzevich <steven@duz.ie>
2022-08-13 18:12:05 +1000

    better modularisation of lib

--- a3/lib.js
+++ a3/lib.js
@@ -1,3 +1,8 @@
+import { formValidate } from './script.js';
+
+// this file contains only pre-existing (from A2) javascript code.
+// some of this code has been extracted from the inline scripts that were used in A2.
+
 // event listener that controls the display status of the go-to-top-button (show only if scroll position is passed the navbar, and don't block the footer)
 document.addEventListener(
   'scroll',
@@ -17,7 +22,7 @@ document.addEventListener(
 );
 
 // movies dictionary
-export const movies = {
+const movies = {
   ACT: {
     name: 'Top Gun: Maverick',
     rating: 'PG-13',
@@ -81,7 +86,7 @@ export const movies = {
 };
 
 // seat prices dictionary
-export const prices = {
+const prices = {
   STA: {
     name: 'Standard Adult',
     normal: '20.50',
@@ -114,12 +119,12 @@ export const prices = {
   }
 };
 
-export function updatePrices(isChecked) {
+function updatePrices(isChecked) {
   if (isChecked) {
     for (let code in prices) {
       document.getElementById(
         code
-      ).innerHTML = `${prices[code].name} (${code})<br><span style="font-size:1.5em">$${prices[code].discounted}</span>`;
+      ).innerHTML = `${prices[code].name}<br><span style="font-size:1.5em">$${prices[code].discounted}</span>`;
     }
     document.getElementById('discount-notice').innerHTML =
       'Discounts only apply to 12pm weekday matinée sessions, and all day Monday.';
@@ -127,13 +132,13 @@ export function updatePrices(isChecked) {
     for (let code in prices) {
       document.getElementById(
         code
-      ).innerHTML = `${prices[code].name} (${code})<br><span style="font-size:1.5em">$${prices[code].normal}</span>`;
+      ).innerHTML = `${prices[code].name}<br><span style="font-size:1.5em">$${prices[code].normal}</span>`;
     }
     document.getElementById('discount-notice').innerHTML = '';
   }
 }
 
-export function isDiscounted(session) {
+function isDiscounted(session) {
   if (session.includes('Mon')) return true;
   if (
     session.includes('12pm') &&
@@ -142,4 +147,179 @@ export function isDiscounted(session) {
   )
     return true;
   return false;
+}
+
+export function initPrices() {
+  var showDiscountedPrices = document.getElementById(
+    'show-discounted-prices-checkbox'
+  );
+
+  updatePrices(showDiscountedPrices.checked);
+
+  // toggle between discounted and full seat prices on checkbox change
+  showDiscountedPrices.addEventListener('change', function () {
+    updatePrices(this.checked);
+  });
+}
+
+export function initMovies() {
+  var movieShowtimesContainer = document.getElementById('movie-cards');
+
+  for (let code in movies) {
+    let movie = movies[code];
+    let movieCard = document.createElement('div');
+    movieCard.className = 'flip-card';
+    movieCard.innerHTML = `
+          <div class="flip-card-content">
+            <div class="flip-card-front">
+              <div class='flip-card-info'><h3 id='flip-card-name'>${
+                movie.name
+              } <sub>(${movie.rating})</sub></h3></div>
+              <img class='flip-card-poster' src="../../media/${
+                movie.posterURL
+              }" alt="Movie poster for ${movie.name}">
+            </div>
+            <div class="flip-card-back">
+              <h2 id='flip-card-back-h2'>${movie.name} <sub>(${
+      movie.rating
+    })</sub></h2>
+              <h3>Synopsis</h3>
+              <p>${movie.synopsis}</p>
+              <h3>Session times</h3>
+              <p>${movie.showings.join('<br>')}</p>
+              <a class='btn' href='booking.php?movie=${code}'>Buy Tickets</a>
+            </div>
+          </div>
+        `;
+    movieShowtimesContainer.appendChild(movieCard);
+  }
+}
+
+export function initMovieDetails($_GET) {
+  const movie = movies[$_GET.movie];
+  const movieInfoDiv = document.getElementById('movie-info');
+  const bookingDiv = document.getElementById('book');
+
+  movieInfoDiv.innerHTML = `
+        <div id="movie-info-heading">
+          <h2 id='name'>${movie.name} <sub>(${movie.rating})</sub></h2>
+        </div>
+
+        <div id='movie-media-container'>
+          <img id='booking-poster' src="../../media/${
+            movie.posterURL
+          }" alt="Movie poster for ${movie.name}">
+          <div id='trailer-container'>
+            <iframe id="trailer" src="https://www.youtube.com/embed/${
+              movie.trailer
+            }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
+
+        <p id="synopsis">${movie.synopsis}</p>
+        <div id="movie-detail-cards" class="card-container">
+          <div class="movie-detail-card card-template"><h3>Director</h3><p id="director">${
+            movie.director
+          }</p></div>
+          <div class="movie-detail-card card-template"><h3>STARS</h3><p id="actors">${
+            movie.actors
+          }</p></div>
+          <div class="movie-detail-card card-template"><h3>Showtimes</h3><p id="showings">${movie.showings.join(
+            ', '
+          )}</p></div>
+        </div>
+      `;
+
+  let dayRadios = '';
+  for (let i = 0; i < movie.showings.length; i++) {
+    dayRadios += `
+          <input class='price-variable-input' type="radio" name="day" value="${movie.showings[
+            i
+          ]
+            .substr(0, 3)
+            .toUpperCase()}" id="day-${i}" ${
+      i === 0 ? 'checked' : ''
+    } data-pricing="${
+      isDiscounted(movie.showings[i]) ? 'discprice' : 'fullprice'
+    }">
+          <label class='btn day-radio-label' for="day-${i}">${
+      movie.showings[i]
+    }</label>
+        `;
+  }
+
+  let seatSelects = '';
+  for (let code in prices) {
+    seatSelects += `
+          <label class='seat-selection-label' for="seats-${code}">${prices[code].name} Seats:</label>
+          <select class='price-variable-input seat-selector' name="seats[${code}]" id="seats-${code}" data-fullprice="${prices[code].normal}" data-discprice="${prices[code].discounted}">
+            <option value="">Please Select</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select><br>
+        `;
+  }
+
+  bookingDiv.innerHTML = `
+        <h2>Book now</h2>
+        <form id='booking-form' action='booking.php?movie=${$_GET.movie}' method='post'>
+          <input type='hidden' name='movie' value='${$_GET.movie}' />
+
+          <input type='text' id='booking-name-field' name='user[name]' value='' placeholder='name' required/>
+          <p class='error' id='nameError'></p>
+
+          <input type='text' name='user[email]' value='' placeholder='email' required/>
+
+          <input type='text' id='booking-mobile-field' name='user[mobile]' value='' placeholder='mobile' required />
+          <p class='error' id='mobileError'></p>
+          
+          <h3>Select a session</h3>
+          ${dayRadios}
+          <h3>Add seats</h3>
+          ${seatSelects}
+          <input id="submit" class='btn' type='submit' value="submit" />
+          <label class='btn' for='submit'>Submit</label>
+          <h3 id='booking-price'></p>
+          <p class='error' id='selectionError'></p>
+        </form>
+      `;
+
+  const bookingForm = document.getElementById('booking-form');
+  bookingForm.addEventListener('submit', formValidate);
+
+  const priceDisplay = document.getElementById('booking-price');
+
+  document
+    .querySelectorAll('.price-variable-input')
+    .forEach(function (element) {
+      element.addEventListener('change', function () {
+        priceDisplay.innerHTML = calcPrice();
+      });
+    });
+}
+
+function calcPrice() {
+  const pricing = document.querySelector('input[name="day"]:checked').dataset
+    .pricing;
+
+  const seats = document.querySelectorAll('.seat-selector');
+  let total = 0;
+
+  seats.forEach((element) => {
+    let price = element.dataset[pricing];
+    let quantity = element.value;
+
+    total += price * quantity;
+  });
+
+  if (total == 0) return '';
+  else return `Total: $${Number(total).toFixed(2)}`;
 }
